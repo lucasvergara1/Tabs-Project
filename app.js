9523688a3c23b0ea7b1f299fd569820883757e2c
Lucas <lucas.vergara@live.com>
2024-02-20 00:31:07 -0300

    End of the project

---
+++ app.js
@@ -0,0 +1,23 @@
+const btns = document.querySelectorAll('.tab-btn');
+const about = document.querySelector('.about');
+const articles = document.querySelectorAll('.content');
+
+about.addEventListener('click', function (e) {
+    // console.log(e.target.dataset.id); - comentei para deixar para consultas futuras
+    const id = e.target.dataset.id;
+    if(id){
+        //remove active from other buttons
+        btns.forEach(function(btn){
+            btn.classList.remove('active');
+            e.target.classList.add('active');
+        });
+        //hide other articles
+        articles.forEach(function(articles) {
+            articles.classList.remove('active');
+        });
+        const element = document.getElementById(id);
+        element.classList.add('active');
+
+    };
+
+});
